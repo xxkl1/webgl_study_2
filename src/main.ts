7950169ff0c0c455147b5b5358645e07cceb9e5a
xxkl1 <sanyuelihuaxue@gmail.com>
2023-09-01 00:20:12 +0800

    feat: 添加清除

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { clear } from './utils'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const getGl = function () {
+    const canvas = document.querySelector('#webgl') as HTMLCanvasElement
+    const gl = canvas.getContext('webgl')
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    return {
+        canvas: canvas,
+        gl: gl,
+    }
+}
+
+const __main = function () {
+    const {
+        canvas,
+        gl,
+    } = getGl()
+
+    clear(gl!)
+}
+
+__main()
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+const clear = function (gl: WebGLRenderingContext) {
+    // 设置清除颜色为黑色，不透明
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    /**
+     * 调用之前指定的颜色，清除绘图区域
+     * open gl 基于 多基本缓冲区模型
+     * gl.COLOR_BUFFER_BIT 表示的是 颜色缓冲区
+     * 除了gl.COLOR_BUFFER_BIT外，这里还指定gl.DEPTH_BUFFER_BIT 深度缓冲区， gl.STENCIL_BUFFER_BIT 模板缓冲区
+     * 所以gl.clear(gl.COLOR_BUFFER_BIT)整体表示的就是，清空颜色缓冲区
+     * 换另一种说法是，使用 gl.clearColor设置的颜色，填充颜色缓冲区
+     */
+    gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+export {
+    clear,
+}
